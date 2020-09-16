671e31baa8d17a4ab5789dce4cdea8b6b2907d01
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-09-16 16:31:08 +0530

    Update RegistrationViewController.swift

--- Harper/Harper/RegistrationViewController.swift
+++ Harper/Harper/RegistrationViewController.swift
@@ -20,8 +20,40 @@ class DestinationViewController: UIViewController{
     }
     
     @IBAction func RegisterUser(_ sender: UIButton) {
-        print(RegistrationPassword.text!)
-        print(RegistrationEmail.text!)
-        print(ConfirmingPassword.text!)
+        if let email = RegistrationEmail.text{
+            if let password = RegistrationPassword.text{
+                if let confirmingpass = ConfirmingPassword.text{
+                    if password == confirmingpass{
+                        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                            if(error != nil){
+                                self.performSegue(withIdentifier: "RegistrationToMainActivity", sender: nil)
+                            } else {
+                                self.showToast(message: "Reg. Failed", font: .systemFont(ofSize: 17.0))
+                                print(error!)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func showToast(message : String, font: UIFont) {
+
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
 }
