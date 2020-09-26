ef936d19080fad775fbb144104cb94dae2548035
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-09-27 00:42:18 +0530

    Connect Buttons to MainActivityController

--- Harper/Harper/MainViewController.swift
+++ Harper/Harper/MainViewController.swift
@@ -10,5 +10,23 @@ import UIKit
 import Firebase
 
 class MainViewController: UIViewController{
+    @IBOutlet weak var Cover_Art: UIImageView!
+    @IBOutlet weak var Song_Title: UITextView!
+    @IBOutlet weak var Artist: UITextView!
+    
+    @IBAction func Play(_ sender: UIButton) {
+    }
+    
+    @IBAction func Next(_ sender: UIButton) {
+    }
+    
+    @IBAction func Previous(_ sender: UIButton) {
+    }
+    
+    @IBAction func Like(_ sender: UIButton) {
+    }
+    
+    @IBAction func ToMenu(_ sender: UIButton) {
+    }
     
 }
