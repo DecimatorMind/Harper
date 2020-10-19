8636fdef339dfcaf41e7873b9144f8133a6c2bec
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-10-19 07:54:40 +0530

    Add Slider Update Code

--- Harper/Harper/DemoCode.swift
+++ Harper/Harper/DemoCode.swift
@@ -65,3 +65,64 @@ import Foundation
 //let seconds = Float(CMTimeGetSeconds(time))
 //        self.sliderView.duration = duration
 //        self.sliderView.progress = seconds / Float(duration)
+
+
+// Final Slider Value update
+
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Pranjal Bhardwaj on 19/10/20.
+//
+//
+//import UIKit
+//import AVFoundation
+//
+//class ViewController: UIViewController {
+//
+//    @IBOutlet weak var Slider: UISlider!
+//
+//    var player = AVAudioPlayer()
+//    var timer = Timer()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let path = Bundle.main.path(forResource: "Let_Her_Go", ofType: "mp3")!
+//        let url = URL(fileURLWithPath: path)
+//        do{
+//            player = try AVAudioPlayer(contentsOf: url)
+//        } catch{
+//            print("Error found")
+//        }
+//        player.prepareToPlay()
+//
+//        Slider.maximumValue = Float(player.duration)
+//    }
+//
+//    @IBAction func Play(_ sender: Any) {
+//        if(player.isPlaying){
+//            player.pause()
+//            timer.invalidate()
+//        } else {
+//            player.play()
+//        }
+//        startTimer()
+//    }
+//
+//    func startTimer(){
+//        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.update(_:)), userInfo: nil,repeats: true)
+//        timer.fire()
+//    }
+//
+//    @objc func update(_ timer: Timer){
+//        Slider.value = CFloat(player.currentTime)
+//    }
+//
+//    deinit {
+//           timer.invalidate()
+//    }
+//
+//}
+
+
