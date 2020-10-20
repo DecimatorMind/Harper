c3384feaa17b41e6a736972c55a3a9436a2744ec
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-10-20 06:16:44 +0530

    Update DemoCode.swift

--- Harper/Harper/DemoCode.swift
+++ Harper/Harper/DemoCode.swift
@@ -8,65 +8,6 @@
 
 import Foundation
 
-// Demo Code from Stack Overflow
-
-
-//func keepTrackOfAudio() {
-//    audioPlayer?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
-//
-//    //track progress
-//
-//    let interval = CMTime(value: 1, timescale: 2)
-//    audioPlayer?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main , using: { (progressTime) in
-//        let seconds = CMTimeGetSeconds(progressTime)
-//        let secondsString = String(format: "%02d", Int(seconds.truncatingRemainder(dividingBy: 60)))
-//        let minutesString = String(format: "%02d", Int(seconds / 60))
-//
-//        self.audioCurrentTimeLabel.text = "\(minutesString):\(secondsString)"
-//
-//        //lets move the slider thumb
-//        if let duration = self.audioPlayer?.currentItem?.duration {
-//            let durationSeconds = CMTimeGetSeconds(duration)
-//
-//            self.audioSlider.value = Float(seconds / durationSeconds)
-//
-//        }
-//    })
-//
-//}
-//
-
-
-// Slider Timer Update
-
-
-//override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//
-//
-//        if let duration = audioPlayer?.currentItem?.duration {
-//            let seconds = CMTimeGetSeconds(duration)
-//
-//            let secondsText = String(format: "%02d", Int(seconds) % 60)
-//            let minutesText = String(format: "%02d", Int(seconds) / 60)
-//            audioLengthLabel.text = "\(minutesText):\(secondsText)"
-//        }
-//
-//    }
-//}
-
-// Timer function to mark the time of the slider
-
-//Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateAudioProgressView), userInfo: nil, repeats: true)
-//  progressView.setProgress(Float(audioPlayer.currentTime/audioPlayer.duration), animated: false)
-
-
-// Pandora Player Code Bit
-
-//let seconds = Float(CMTimeGetSeconds(time))
-//        self.sliderView.duration = duration
-//        self.sliderView.progress = seconds / Float(duration)
-
-
 // Final Slider Value update
 
 //
