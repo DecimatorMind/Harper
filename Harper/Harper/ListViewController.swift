95eb84658f75df800872f6276fe9a4b7243f112a
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-11-07 15:38:35 +0530

    Add List View

--- Harper/Harper/ListViewController.swift
+++ Harper/Harper/ListViewController.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
-class SongListViewController: UITableViewController{
+class SongListViewController: UIViewController {
+    
+    let SongTitles = ["Starboy","No Idea","Out West","Franchise"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension SongListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+extension SongListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SongTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        cell.textLabel?.text = SongTitles[indexPath.row]
+        return cell
+    }
     
 }
